66c3a709d67409a54ac9ae1e7dda318a03de614e
Maarten Ringburg <maarten.ringburg@gmail.com>
2025-10-04 18:36:01 +0400

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/maartenringburg/Downloads/Pathoscope-V5/venv"
+set -gx VIRTUAL_ENV "/Users/maartenringburg/Desktop/Pathoscope-V5/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
